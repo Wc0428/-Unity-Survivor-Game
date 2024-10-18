42809241018a16a383d7198b5b1c0c29221db3f2
Wc0428 <109028745+Wc0428@users.noreply.github.com>
2024-10-18 15:38:50 +0800

    fix

--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -9,7 +9,6 @@ public class SelectionManager : MonoBehaviour
     public GameObject interaction_Info_UI;
     Text interaction_text;
     
-    
     private void Start()
     {
         interaction_text = interaction_Info_UI.GetComponent<Text>();
